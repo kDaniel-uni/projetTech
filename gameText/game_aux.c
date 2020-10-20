8d3b890ab3b2b3e8101eb7e1141f07d05586d527
Karl <karl.daniel@etu.u-bordeaux.fr>
2020-10-20 22:30:05 +0200

    CABUILD

--- gameText/game_aux.c
+++ gameText/game_aux.c
@@ -1,9 +1,68 @@
-#ifndef __GAME_AUX_C__
-#define __GAME_AUX_C__
 #include "game.h"
+#include <stdio.h>
 
-void game_print(cgame g);
-game game_default(void);
-game game_default_solution(void);
 
-#endif 
+void game_print(cgame g){
+
+    printf("   01234567\n");
+    printf("   --------\n");
+    for (int i=0; i<8; i++){
+        printf("%d |",i);
+        for (int j = 0; j<8; j++){
+            switch(game_get_square(g,i,j)){
+                case EMPTY : printf(" "); break;
+                case TREE : printf("x"); break;
+                case TENT : printf("*"); break;
+                case GRASS : printf("-"); break;
+            }
+        }
+        printf("| %u\n",game_get_expected_nb_tents_row(g,i));   
+    }
+    printf("   --------\n");
+    printf("   ");
+    for (int j = 0; j<8; j++){
+        printf("%u",game_get_expected_nb_tents_col(g,j));
+    }
+    printf("\n");
+
+}
+
+game game_default(void){
+    square sqr[64] = {
+        EMPTY,EMPTY,EMPTY,EMPTY,TREE,TREE,EMPTY,EMPTY,
+        TREE,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,TREE,
+        EMPTY,EMPTY,EMPTY,EMPTY,TREE,EMPTY,EMPTY,EMPTY,
+        TREE,EMPTY,EMPTY,EMPTY,EMPTY,TREE,EMPTY,EMPTY,
+        EMPTY,TREE,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,
+        TREE,EMPTY,EMPTY,EMPTY,TREE,EMPTY,TREE,EMPTY,
+        EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,
+        TREE,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY
+    };
+
+    uint tents_in_each_row[8] = {3,0,4,0,4,0,1,0};
+
+    uint tents_in_each_col[8] = {4,0,1,2,1,1,2,1};
+
+    return game_new(*sqr,*tents_in_each_row,*tents_in_each_col);
+
+};
+
+game game_default_solution(void){
+    square sqr[64] = {
+        TENT,GRASS,GRASS,TENT,TREE,TREE,TENT,GRASS,
+        TREE,GRASS,GRASS,GRASS,GRASS,GRASS,GRASS,TREE,
+        TENT,GRASS,GRASS,TENT,TREE,TENT,GRASS,TENT,
+        TREE,GRASS,GRASS,GRASS,GRASS,TREE,GRASS,GRASS,
+        TENT,TREE,TENT,GRASS,TENT,GRASS,TENT,GRASS,
+        TREE,GRASS,GRASS,GRASS,TREE,GRASS,TREE,GRASS,
+        TENT,GRASS,GRASS,GRASS,GRASS,GRASS,GRASS,GRASS,
+        TREE,GRASS,GRASS,GRASS,GRASS,GRASS,GRASS,GRASS
+    };
+
+    uint tents_in_each_row[8] = {3,0,4,0,4,0,1,0};
+
+    uint tents_in_each_col[8] = {4,0,1,2,1,1,2,1};
+
+    return game_new(*sqr,*tents_in_each_row,*tents_in_each_col);
+
+};
--- gameText/game_text.c
+++ gameText/game_text.c
@@ -1,3 +1,43 @@
+#include "game_aux.h"
+#include <stdio.h>
+
 int main(void) { 
+
+    game g = game_default();
+
+    while (!game_is_over(g)){
+
+        game_print(g);
+
+        int x,y,z;
+        int ret = scanf("%c %d %d", &x, &y, &z);
+
+        if (ret == 1){
+
+            if (x == 'h'){
+                printf("Tapez r pour réinitialier, q pour quitter, ou la commande de la forme <c> <i> <j> pour placer dans la case (<i>,<j>) une tente (<c> == 't'), ou de l'herbe (<c> == 'g') ou une case vide (<c> == 'e')");
+            } else if (x == 'r') {
+                game_restart(g);
+            } else if (x == 'q') {
+                game_delete(g);
+            } else {
+                printf("Caractère invalide, tapez h pour afficher l'aide");
+            }
+        } else if (ret == 3) {
+            if (game_check_move(g,y,z,x) == REGULAR){
+                game_play_move(g,y,z,x);
+            } else if (game_check_move(g,y,z,x) == ILLEGAL) {
+                printf("Position et/ou type de case invalide");
+            } else {
+                game_play_move(g,y,z,x);
+                printf("shame");
+                return 0;
+            } 
+        } else {
+            printf("Mauvaise commande, tapez h pour afficher l'aide");
+        }    
+    }
+    printf("congratulation");
+
     return 0;
 }
