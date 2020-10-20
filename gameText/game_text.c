f2a36cf76f09e7e9f58fe9625e22f8bb49fb4c1e
Karl <karl.daniel@etu.u-bordeaux.fr>
2020-10-20 23:06:11 +0200

    aze

--- gameText/game_text.c
+++ gameText/game_text.c
@@ -11,9 +11,11 @@ int main(void) {
 
         char x;
         int y,z;
-        int ret = scanf("%c %d %d", &x, &y, &z);
 
-        if (ret == 1){
+        printf("Donnez un caractère");
+        int cara = scanf("%c",&x);
+
+        if (cara == 1){
 
             if (x == 'h'){
                 printf("Tapez r pour réinitialier, q pour quitter, ou la commande de la forme <c> <i> <j> pour placer dans la case (<i>,<j>) une tente (<c> == 't'), ou de l'herbe (<c> == 'g') ou une case vide (<c> == 'e')\n");
@@ -21,18 +23,26 @@ int main(void) {
                 game_restart(g);
             } else if (x == 'q') {
                 game_delete(g);
+                printf("shame\n");
+                return 0;
             } else {
                 printf("Caractère invalide, tapez h pour afficher l'aide\n");
             }
-        } else if (ret == 3) {
+        } else {
+            printf("Caractère invalide, tapez h pour afficher l'aide\n"); 
+        }
+        if ( x == 't' || x == 'e' || x == 'g') {
+
+            printf("Donnez la position de la case");
+            int ret = scanf("%d %d",&y,&z);
+
             if (game_check_move(g,y,z,x) == REGULAR){
                 game_play_move(g,y,z,x);
             } else if (game_check_move(g,y,z,x) == ILLEGAL) {
                 printf("Position et/ou type de case invalide\n");
             } else {
                 game_play_move(g,y,z,x);
-                printf("shame\n");
-                return 0;
+                printf("Warning : Losing move\n");
             } 
         } else {
             printf("Mauvaise commande, tapez h pour afficher l'aide\n");
