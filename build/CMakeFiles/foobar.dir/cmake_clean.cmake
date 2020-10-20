add20ab45bc7e0b61db672bb9c61b7e995abd0a1
Karl Daniel <kdaniel@emi.u-bordeaux.fr>
2020-10-20 17:43:07 +0200

    add_previous_td

---
+++ build/CMakeFiles/foobar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foobar.dir/bar.c.o"
+  "CMakeFiles/foobar.dir/foo.c.o"
+  "libfoobar.a"
+  "libfoobar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/foobar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld.dir/bar.c.o"
+  "CMakeFiles/helloworld.dir/foo.c.o"
+  "CMakeFiles/helloworld.dir/main.c.o"
+  "helloworld"
+  "helloworld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
